aee30a97862f40b508b3efa50b50532174d64529
Frederic Le Bris <flebris@gmail.com>
2015-10-07 08:16:53 +0200

    add xls and var template

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_mail import Mail, Message
+from flask.ext import excel
 from pdfs import create_pdf
-# ...
+
+import pyexcel.ext.xls 
+
+import xlwt
+import StringIO
+import mimetypes
+from flask import Response
+from werkzeug.datastructures import Headers
+
 
 app = Flask(__name__)
 
@@ -13,7 +22,7 @@ app.config.update(
 	MAIL_USE_SSL=False,
 	MAIL_USE_TLS = True,
 	MAIL_USERNAME = 'flebris@outlook.com',
-	MAIL_PASSWORD = 'password_test'
+	MAIL_PASSWORD = 'cspr!3204'
 	)
 
 mail = Mail()
@@ -39,15 +48,86 @@ def index():
                   sender="flebris@outlook.com",
                   recipients=["flebris@gmail.com"])
  	msg.body = "This is the email body"
- 	pdf = create_pdf(render_template('template.html'))
+ 	pdf = create_pdf(render_template('template.html', name='fred2'))
  	msg.attach("file.pdf", "application/pdf",  pdf.getvalue())
  	mail.send(msg)
  	return 'Sent'
 
+@app.route("/download", methods=['GET'])
+def download_file():
+	array = [[1,2], [3, 4]]
+	return excel.make_response_from_array(array, "xls", status=200)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+
+#... code for setting up Flask
+
+@app.route('/export/')
+def export_view():
+    #########################
+    # Code for creating Flask
+    # response
+    #########################
+    response = Response()
+    response.status_code = 200
+
+
+    ##################################
+    # Code for creating Excel data and
+    # inserting into Flask response
+    ##################################
+    workbook = xlwt.Workbook()
+    ws = workbook.add_sheet('A Test Sheet')
+    ws.write(0, 0, 1234.56)
+    ws.write(1, 0, 3)
+    ws.write(2, 0, 1)
+    ws.write(2, 1, 1)
+    ws.write(2, 2, xlwt.Formula("A3+B3"))
+
+    #.... code here for adding worksheets and cells
+
+    output = StringIO.StringIO()
+    workbook.save(output)
+    response.data = output.getvalue()
+
+    ################################
+    # Code for setting correct
+    # headers for jquery.fileDownload
+    #################################
+    filename = "test.xls"
+    mimetype_tuple = mimetypes.guess_type(filename)
+
+    #HTTP headers for forcing file download
+    response_headers = Headers({
+            'Pragma': "public",  # required,
+            'Expires': '0',
+            'Cache-Control': 'must-revalidate, post-check=0, pre-check=0',
+            'Cache-Control': 'private',  # required for certain browsers,
+            'Content-Type': mimetype_tuple[0],
+            'Content-Disposition': 'attachment; filename=\"%s\";' % filename,
+            'Content-Transfer-Encoding': 'binary',
+            'Content-Length': len(response.data)
+        })
+
+    if not mimetype_tuple[1] is None:
+        response.update({
+                'Content-Encoding': mimetype_tuple[1]
+            })
+
+    response.headers = response_headers
+
+    #as per jquery.fileDownload.js requirements
+    response.set_cookie('fileDownload', 'true', path='/')
+
+    ################################
+    # Return the response
+    #################################
+    return response
+
 
 if __name__ == '__main__':
 	app.debug = True
